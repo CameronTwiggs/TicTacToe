67aee3d2741e18fd10e37bb3b61c1042733e2573
Cameron Twiggs <camerontwiggs@AdminisatorsMBP.attlocal.net>
2019-08-25 23:58:25 -0400

    add Tic

---
+++ build/precache-manifest.732ecefb9a90089830bac0c866d36028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb544bb47701650f1c39b155353fd067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1cbb3518549e619bb8",
+    "url": "/static/css/main.0cf12edd.chunk.css"
+  },
+  {
+    "revision": "b9501e570bf14ea1c389",
+    "url": "/static/js/2.bf5c1f19.chunk.js"
+  },
+  {
+    "revision": "ab1cbb3518549e619bb8",
+    "url": "/static/js/main.eaeac792.chunk.js"
+  },
+  {
+    "revision": "6154302529968ada5372",
+    "url": "/static/js/runtime~main.73b90d13.js"
+  }
+]);
