921822f14cc4e2b7f8cdb7b73ea814eff43df916
Cameron Twiggs <cameron.020617@gmail.com>
2019-12-16 23:38:11 -0500

    fixes vuneralbies and also installs gh-pages

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -24,7 +24,7 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square 
+    return <Square
     value={this.state.squares[i]}
     onClick={()=>this.handleClick(i)} />;
   }
@@ -82,5 +82,6 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
 
 export default Board;
